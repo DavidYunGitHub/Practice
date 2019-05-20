632cfca6ef932a64a55dc952f44edebf81cc50c7
codemyburger <ff7yeah@yahoo.com>
2019-05-20 15:59:09 -0500

    Add files via upload

---
+++ function.js
@@ -0,0 +1,45 @@
+//create a basic function call hello
+
+//once a function is executed, it "dies"
+
+// function hello(fname) {
+
+//         console.log(typeof(fname))
+//         console.log(fname)
+//         console.log('Hello')
+
+// }
+
+// var name = "Veronica"
+// var helloVariable = hello(name);
+
+// console.log(helloVariable);
+
+// function add(a,b) {
+//     console.log(a + b)
+// }
+
+// var a = 4;
+// var b = 6;
+
+// var resultofAdd = add(a, b);
+
+// function addArray(arr){
+
+//     console.log(arr[2])
+
+//     arr.forEach(function(element){
+//         console.log(element)
+//     });
+
+//     arr.push(10)
+// }
+
+// var myArray = [1,4,7,9,12,4];
+
+// var result = addArray(myArray);
+// console.log(result)
+
+var name  = "Kevin";
+
+function greeting
